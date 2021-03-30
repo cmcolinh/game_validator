bfeb9ed053d2cd917099d51565399335bac151a3
Colin Horner <25807014+cmcolinh@users.noreply.github.com>
2021-03-30 10:31:36 +0900

    Refactor Types:Callable, add a few set Array types

--- lib/game_validator.rb
+++ lib/game_validator.rb
@@ -4,7 +4,12 @@ module Types
   include Dry.Types()
 end
 callable = Types.Interface(:call)
-Types::Callable = Types.Constructor(Method){|value| callable.call(value); value.method(:call)}
+Types::Callable = Types.Constructor(Method) do |value|
+  callable.call(value) # raises Dry::Types::ConstraintError if object doesn't respond to #call
+  value.is_a?(Method) || value.is_a?(Proc) ? value : value.method(:call)
+end
+Types::ArrayOfCallable = ::Types::Array::of(::Types::Callable)
+Types::ArrayOfStrictInteger = ::Types::Array::of(::Types::Strict::Integer)
 
 require 'game_validator/version'
 require 'game_validator/validator'
